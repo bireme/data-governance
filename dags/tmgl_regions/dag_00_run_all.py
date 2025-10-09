308e7547936409e161815cb652bc0665e656c31e
Diego F Ucha <diegoucha@reachsolutions.com.br>
2025-10-09 14:22:56 -0300

    changes to run all in tmgl region

--- dags/tmgl_regions/dag_00_run_all.py
+++ dags/tmgl_regions/dag_00_run_all.py
@@ -22,84 +22,98 @@ with DAG(
         task_id='run_TMGL_REGION_01_setup',
         trigger_dag_id='TMGL_REGION_01_setup',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_complementary = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_complementary',
         trigger_dag_id='TMGL_REGION_02_create_metric_complementary',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_dimentions = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_dimentions',
         trigger_dag_id='TMGL_REGION_02_create_metric_dimentions',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_doctype = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_doctype',
         trigger_dag_id='TMGL_REGION_02_create_metric_doctype',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_journals = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_journals',
         trigger_dag_id='TMGL_REGION_02_create_metric_journals',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_languages = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_languages',
         trigger_dag_id='TMGL_REGION_02_create_metric_languages',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_map = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_map',
         trigger_dag_id='TMGL_REGION_02_create_map',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_regions = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_regions',
         trigger_dag_id='TMGL_REGION_02_create_metric_regions',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_studytype = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_studytype',
         trigger_dag_id='TMGL_REGION_02_create_metric_studytype',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_subjects = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_subjects',
         trigger_dag_id='TMGL_REGION_02_create_metric_subjects',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_therapies = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_therapies',
         trigger_dag_id='TMGL_REGION_02_create_metric_therapies',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_timeline = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_timeline',
         trigger_dag_id='TMGL_REGION_02_create_timeline',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_create_metric_traditional = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_02_create_metric_traditional',
         trigger_dag_id='TMGL_REGION_02_create_metric_traditional',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_export_html = TriggerDagRunOperator(
         task_id='run_TMGL_REGION_03_export_html',
         trigger_dag_id='TMGL_REGION_03_export_html',
         wait_for_completion=True,
+        deferrable=True,
     )
 
     run_setup >> run_create_metric_complementary >> run_export_html
